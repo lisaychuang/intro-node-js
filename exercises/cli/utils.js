1fb9303d800cdee807f74f813f98d7ea25c11af9
Lisa Huang <lisa.yc.huang@gmail.com>
2019-01-10 14:04:10 -0800

    Exercise 3 solution

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const path = require('path')
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
-
+const contactsLocation = path.join(__dirname, 'contacts.json');
 /**
  * should read the contacts at the
  * @contactsLocation path and convert
  * it to a js object
  */
 const getContacts = () => {
-  
+  const contacts = fs.readFileSync(contactsLocation).toString();
+  return JSON.parse(contacts);
 }
 
 /**
@@ -19,7 +19,7 @@ const getContacts = () => {
  * @param {Object} contacts contacts object
  */
 const saveContacts = (contacts) => {
-
+  fs.writeFileSync(contactsLocation, JSON.stringify(contacts, null, 2))
 }
 
 module.exports = {
